65a9664d2d53c2de464b62faf4c7fc44b37340bf
Janle <J_janle@163.com>
2019-03-15 08:01:03 +0800

    提交pom相关的操作和控制demo

--- passport-demo/src/main/java/com/ljz/demo/DemoApplication.java
+++ passport-demo/src/main/java/com/ljz/demo/DemoApplication.java
@@ -1,5 +1,10 @@
 package com.ljz.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by ljz07 on 2019/3/15.
  */
