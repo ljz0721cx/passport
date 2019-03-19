7682249b235f44576cd4b5b56a661ecb07d428c3
Janle <j_janle@163.com>
2019-03-19 19:43:07 +0800

    测试项目中用户登录体系的加解密

--- README.md
+++ README.md
@@ -7,17 +7,18 @@ wireMock 快速伪造restfull服务
 
 # 依赖管理
 passport
-    -passport-app 手机端登录拦截
-    -passport-browser 浏览器登录拦截
-    -passport-core 核心安全包
-    -passport-demo 样例使用
+    -passport-app 手机端登录拦截  
+    -passport-browser 浏览器登录拦截  
+    -passport-core 核心安全包  
+    -passport-demo 样例使用  
+    -passport-test 安全登录项目
     
 # 配置相关控制
  --------------------------
- 顺序依次如下
-     -----filter----  
-     -----interceptor----   
-     -----ControllerAdvice----  
-     -----Aspect----  
-     -----Controller----  
-     -----代码执行------
+ 顺序依次如下  
+     -----filter----对请求URL的过滤     
+     -----interceptor----对请求方法调用的处理   
+     -----ControllerAdvice----对控制层的监控  
+     -----Aspect----对接口的切面  
+     -----Controller----控制层实现  
+     -----代码执行------具体代码执行
--- passport-browser/src/main/java/com/ljz/passport/config/BrowssrSecurityConfig.java
+++ passport-browser/src/main/java/com/ljz/passport/config/BrowssrSecurityConfig.java
@@ -1,26 +1,49 @@
 package com.ljz.passport.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
+ * web应用适配器的配置
+ *
  * @author 李建珍
  * @date 2019/3/19
  */
 @Configuration
 public class BrowssrSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    /**
+     * 选择通用的加密方式
+     *
+     * @return
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * 参数是httpsecurity
+     *
+     * @param http
+     * @throws Exception
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         //用表单认证所有的访问都需要认证
-        //http.formLogin()
-        http.httpBasic()
+        http.formLogin()
+                //httpbasic认证方式
+                //http.httpBasic()
                 .and()
+                //对请求授权
                 .authorizeRequests()
-                //所有请求求需要认证
+                //任何请求
                 .anyRequest()
-                //
+                //都需要身份认证
                 .authenticated();
     }
 }
