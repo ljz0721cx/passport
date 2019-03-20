2d2fd83bc4cc1994168c1f304ba27a7f969ebc9b
Janle <j_janle@163.com>
2019-03-21 00:10:45 +0800

    重写redirect请求的测试。 将测试项目中的代码补充完整。

--- passport-test/src/main/java/com/ljz/passport/test/web/user/UserController.java
+++ passport-test/src/main/java/com/ljz/passport/test/web/user/UserController.java
@@ -1,21 +1,45 @@
 package com.ljz.passport.test.web.user;
 
 import com.fasterxml.jackson.annotation.JsonView;
+import com.ljz.passport.test.dto.User;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import com.ljz.passport.test.dto.User;
 /**
  * @author 李建珍
  * @date 2019/3/17
  */
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
-    @RequestMapping(value = "/user", method = RequestMethod.GET)
+
+    /* @GetMapping("/me")
+     public Object getMe() {
+         return SecurityContextHolder.getContext().getAuthentication();
+     }*/
+    /*@GetMapping("/me")
+    public Object getMe(Authentication authentication) {
+        return authentication;
+    }*/
+    @GetMapping("/me")
+    public Object getMe(@AuthenticationPrincipal UserDetails userDetails) {
+        return userDetails;
+    }
+
+    /**
+     * 查询用户的信息列表
+     *
+     * @return
+     */
+    @RequestMapping(value = "", method = RequestMethod.GET)
     @JsonView(User.class)
     public List<User> list() {
         List<User> users = new ArrayList<>();
